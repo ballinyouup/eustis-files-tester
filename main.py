e8066b314873d924c330175de33f37bd0ba4ba82
ballinyouup <bryanhuici@gmail.com>
2026-01-30 23:41:46 -0500

    fixed file path

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main():
     if selected_option == "Upload a folder":
         folder_upload = input("Enter the path of the folder you want to upload: ")
         folder_eustis = input("Enter the path to store folder on eustis. '/' for root directory : ")
-        result = subprocess.run(['scp', '-r', f"{folder_upload}" , f"{user}@eustis.eecs.ucf.edu:{folder_eustis}"], capture_output=True, text=True)
+        result = subprocess.run(['scp', '-r', f"{folder_upload}" , f"{user}@eustis.eecs.ucf.edu:~{folder_eustis}"], capture_output=True, text=True)
         print("Uploaded Folder!" + result.stdout)
     elif selected_option == "Upload a file":
         file_upload = input("Enter the path of the file you want to upload: ")
         file_eustis = input("Enter the path to store file on eustis. '/' for root directory: ")
-        result = subprocess.run(['scp', f"{file_upload}" , f"{user}@eustis.eecs.ucf.edu:{file_eustis}"], capture_output=True, text=True)
+        result = subprocess.run(['scp', f"{file_upload}" , f"{user}@eustis.eecs.ucf.edu:~{file_eustis}"], capture_output=True, text=True)
         print("Uploaded file!" + result.stdout)
     elif selected_option == "Connect to Eustis":
         print("Connecting to Eustis...")
